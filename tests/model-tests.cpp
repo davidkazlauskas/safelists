6dd13f15e525663aab972a0267cc6e084e4a3fc3
David <david@templatious.org>
2015-07-20 19:31:58 +0300

    BOOM

--- tests/model-tests.cpp
+++ tests/model-tests.cpp
@@ -35,12 +35,7 @@ TEST_CASE("model_basic_sqlite","[model]") {
 
     {
         static const char* query =
-            "CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT);"
-            "INSERT INTO Friends(Name) VALUES ('Tom');"
-            "INSERT INTO Friends(Name) VALUES ('Rebecca');"
-            "INSERT INTO Friends(Name) VALUES ('Jim');"
-            "INSERT INTO Friends(Name) VALUES ('Roger');"
-            "INSERT INTO Friends(Name) VALUES ('Robert');";
+            "SELECT Name FROM FRIENDS;";
 
         std::vector< std::string > friendos;
 
@@ -48,9 +43,9 @@ TEST_CASE("model_basic_sqlite","[model]") {
              AsyncSqlite::Execute, const char*, std::function<void(int,char**,char**)>
         >(
             nullptr, query,
-                [&](int cnt,char** headers,char** values) {
-                    SA::add(friendos,values[0]);
-                }
+            [&](int cnt,char** headers,char** values) {
+                SA::add(friendos,values[0]);
+            }
         );
         msg->message(pack);
 

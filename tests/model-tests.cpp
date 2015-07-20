4a1cae204ad44e84274fe7e31dea35235f55c5f3
David <david@templatious.org>
2015-07-20 19:02:17 +0300

    sqlite create table

--- tests/model-tests.cpp
+++ tests/model-tests.cpp
@@ -1,9 +1,13 @@
 
 #define CATCH_CONFIG_MAIN
+#include <templatious/FullPack.hpp>
+
 #include "catch.hpp"
 
 #include <model/IndexedListView.hpp>
 #include <model/AsyncSqlite.hpp>
+
+TEMPLATIOUS_TRIPLET_STD;
 
 using namespace SafeLists;
 
@@ -12,5 +16,13 @@ TEST_CASE("model_string_snapshot","[model]") {
 
 TEST_CASE("model_basic_sqlite","[model]") {
     auto msg = AsyncSqlite::createNew(":memory:");
+
+    {
+        auto pack = SF::vpackPtrCustom< templatious::VPACK_WAIT,
+             AsyncSqlite::Execute, const char*
+        >(
+            nullptr, ""
+        );
+    }
 }
 

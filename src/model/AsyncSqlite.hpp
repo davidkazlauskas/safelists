65417c0792d83072759a9ebd708594f1b5c7bfc4
David <david@templatious.org>
2015-07-20 18:10:28 +0300

    starters

---
+++ src/model/AsyncSqlite.hpp
@@ -0,0 +1,8 @@
+#ifndef ASYNCSQLITE_F4E1YMYY
+#define ASYNCSQLITE_F4E1YMYY
+
+namespace SafeLists {
+
+}
+
+#endif /* end of include guard: ASYNCSQLITE_F4E1YMYY */
---
+++ src/model/IndexedListView.hpp
@@ -0,0 +1,14 @@
+
+#ifndef INDEXEDLISTVIEW_LY04U2JN
+#define INDEXEDLISTVIEW_LY04U2JN
+
+namespace SafeLists {
+
+struct ListView {
+
+private:
+};
+
+}
+
+#endif /* end of include guard: INDEXEDLISTVIEW_LY04U2JN */

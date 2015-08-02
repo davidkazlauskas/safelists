7c0324b6efed6c779e54d498a335d2ea8590a314
David <david@templatious.org>
2015-08-02 16:31:54 +0300

    ballin...

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,22 @@
 #include <templatious/FullPack.hpp>
 #include <LuaPlumbing/plumbing.hpp>
 
+struct MyModel : public Gtk::TreeModel {
+
+    Gtk::TreeModelFlags get_flags_vfunc() const override {
+        return Gtk::TREE_MODEL_LIST_ONLY;
+    }
+
+    int get_n_columns_vfunc() const override {
+        return 2;
+    }
+
+    GType get_column_type_vfunc(int index) const override {
+        return G_TYPE_STRING;
+    }
+
+};
+
 struct GtkMainWindow : public Messageable {
 
     GtkMainWindow(Glib::RefPtr<Gtk::Builder>& bld) :
@@ -10,12 +26,32 @@ struct GtkMainWindow : public Messageable {
         _right(nullptr)
     {
         Gtk::Window* outWnd = nullptr;
-        bld->get_widget("applicationwindow1",outWnd);
+        bld->get_widget("window1",outWnd);
         _wnd.reset( outWnd );
 
         bld->get_widget("treeview1",_right);
         bld->get_widget("treeview3",_left);
+
+        _left->append_column_editable("cholo",_mdl.m_col_id);
+        _left->append_column_editable("holo",_mdl.m_col_name);
     }
+
+    class ModelColumns : public Gtk::TreeModel::ColumnRecord {
+    public:
+        ModelColumns() {
+            add(m_col_id);
+            add(m_col_name);
+            add(m_col_foo);
+            add(m_col_number);
+            add(m_col_number_validated);
+        }
+
+        Gtk::TreeModelColumn<unsigned int> m_col_id;
+        Gtk::TreeModelColumn<Glib::ustring> m_col_name;
+        Gtk::TreeModelColumn<bool> m_col_foo;
+        Gtk::TreeModelColumn<int> m_col_number;
+        Gtk::TreeModelColumn<int> m_col_number_validated;
+    };
 
     Gtk::Window& getWindow() {
         return *_wnd;
@@ -31,6 +67,7 @@ private:
     std::unique_ptr< Gtk::Window > _wnd;
     Gtk::TreeView* _left;
     Gtk::TreeView* _right;
+    ModelColumns _mdl;
 };
 
 int main(int argc,char** argv) {

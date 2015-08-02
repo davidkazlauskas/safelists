a69b2db9b33656f66879bbe67a5d04e9fed4f583
David <david@templatious.org>
2015-08-02 15:53:20 +0300

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
 
 #include <gtkmm.h>
 #include <templatious/FullPack.hpp>
+#include <LuaPlumbing/plumbing.hpp>
 
-int main() {
-    auto app = Gtk::Application::create();
+struct GtkMainWindow : public Messageable {
+
+    GtkMainWindow(Glib::RefPtr<Gtk::Builder>& bld) {
+        Gtk::Window* outWnd = nullptr;
+        bld->get_widget("applicationwindow1",outWnd);
+        _wnd.reset( outWnd );
+    }
+
+    Gtk::Window& getWindow() {
+        return *_wnd;
+    }
+
+    void message(templatious::VirtualPack& msg) override {
+    }
+
+    void message(const std::shared_ptr< templatious::VirtualPack >& msg) override {
+    }
+
+private:
+    std::unique_ptr< Gtk::Window > _wnd;
+};
+
+int main(int argc,char** argv) {
+    auto app = Gtk::Application::create(argc,argv);
+
+    auto builder = Gtk::Builder::create();
+    builder->add_from_file("uischemes/main.glade");
+    auto mainWnd = std::make_shared< GtkMainWindow >(builder);
+    app->run(mainWnd->getWindow(),argc,argv);
 }
 

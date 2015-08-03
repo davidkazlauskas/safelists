20f8f50620acc5facd016f2d213f679a3a9882dc
David <david@templatious.org>
2015-08-03 19:08:52 +0300

    write headers

--- src/model/TableSnapshot.cpp
+++ src/model/TableSnapshot.cpp
@@ -1,8 +1,30 @@
 
+#include <templatious/FullPack.hpp>
+
 #include "TableSnapshot.hpp"
+
+TEMPLATIOUS_TRIPLET_STD;
+
+namespace {
+    template <class T>
+    const char* toConstChar(T&& t) {
+        return reinterpret_cast<const char*>(std::addressof(t));
+    }
+}
 
 TableSnapshotBuilder::TableSnapshotBuilder(int columns,char** headerNames) :
     _columns(columns), _totalCount(0)
 {
     _tmp.resize(_columns);
+    // write record count right away, will be edited in the end
+    _stream.write(toConstChar(_totalCount),sizeof(_totalCount));
+    _stream.write(toConstChar(_columns),sizeof(_columns));
+
+    std::string buf;
+    TEMPLATIOUS_0_TO_N(i,columns) {
+        buf = headerNames[i];
+        auto size = buf.size();
+        _stream.write(toConstChar(size),sizeof(size));
+        _stream.write(buf.c_str(),size);
+    }
 }

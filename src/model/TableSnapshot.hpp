f1a6fcf121a9f263e42beaeff2ab5af3f8f89a57
David <david@templatious.org>
2015-08-03 18:45:52 +0300

    snapshot

---
+++ src/model/TableSnapshot.hpp
@@ -0,0 +1,16 @@
+#ifndef TABLESNAPSHOT_5DK8CM2U
+#define TABLESNAPSHOT_5DK8CM2U
+
+struct TableSnapshot {
+
+    int _bufSize;
+    char* _buffer;
+};
+
+struct TableSnapshotBuilder {
+
+    TableSnapshotBuilder(int columns);
+
+};
+
+#endif /* end of include guard: TABLESNAPSHOT_5DK8CM2U */

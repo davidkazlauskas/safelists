3302de8838f218747848b9f68f20e7058bad5cf0
David <david@templatious.org>
2015-08-03 18:55:33 +0300

    dun

--- src/model/TableSnapshot.cpp
+++ src/model/TableSnapshot.cpp
@@ -1,3 +1,8 @@
 
 #include "TableSnapshot.hpp"
 
+TableSnapshotBuilder::TableSnapshotBuilder(int columns):
+    _columns(columns)
+{
+    _tmp.resize(_columns);
+}
--- src/model/TableSnapshot.hpp
+++ src/model/TableSnapshot.hpp
@@ -1,8 +1,13 @@
 #ifndef TABLESNAPSHOT_5DK8CM2U
 #define TABLESNAPSHOT_5DK8CM2U
 
+#include <sstream>
+#include <vector>
+#include <string>
+
 struct TableSnapshot {
 
+private:
     int _bufSize;
     char* _buffer;
 };
@@ -11,6 +16,16 @@ struct TableSnapshotBuilder {
 
     TableSnapshotBuilder(int columns);
 
+    TableSnapshotBuilder() = delete;
+    TableSnapshotBuilder(const TableSnapshotBuilder&) = delete;
+    TableSnapshotBuilder(TableSnapshotBuilder&&) = delete;
+
+    void addRow();
+
+private:
+    int _columns;
+    std::stringstream _stream;
+    std::vector< std::string > _tmp;
 };
 
 #endif /* end of include guard: TABLESNAPSHOT_5DK8CM2U */

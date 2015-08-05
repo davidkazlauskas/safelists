ad240c95c6c1f502a196c40be346183c52366e34
David <david@templatious.org>
2015-08-05 21:11:01 +0300

    ranger carcass

--- src/model/SqliteRanger.hpp
+++ src/model/SqliteRanger.hpp
@@ -14,6 +14,8 @@ private:
     int _actualStart;
     int _actualEnd;
     std::vector< std::vector< std::string > > _valueMatrix;
+    std::function< void(int,int,const char*) > _updateFunction;
+    std::function< const char*(int,int) > _emptyValueFunction;
 };
 
 #endif /* end of include guard: SQLITERANGER_USA0FKBB */

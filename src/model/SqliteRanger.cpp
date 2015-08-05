1a707a2c23beb1a32f1f72f67a9175ba514db028
David <david@templatious.org>
2015-08-05 21:07:33 +0300

    sqlite ranger

---
+++ src/model/SqliteRanger.cpp
@@ -0,0 +1,3 @@
+
+#include "SqliteRanger.hpp"
+
--- src/model/SqliteRanger.hpp
+++ src/model/SqliteRanger.hpp
@@ -2,9 +2,17 @@
 #ifndef SQLITERANGER_USA0FKBB
 #define SQLITERANGER_USA0FKBB
 
+#include <vector>
+
 #include "AsyncSqlite.hpp"
 
 struct SqliteRanger {
+
+private:
+    int _requestedStart;
+    int _requestedEnd;
+    int _actualStart;
+    int _actualEnd;
 
 };
 

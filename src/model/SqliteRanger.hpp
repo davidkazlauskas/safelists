f08e10516061b88dd6806ac6c3cbb0cd2fcd2d9b
David <david@templatious.org>
2015-08-05 21:09:16 +0300

    val matrix

--- src/model/SqliteRanger.hpp
+++ src/model/SqliteRanger.hpp
@@ -13,7 +13,7 @@ private:
     int _requestedEnd;
     int _actualStart;
     int _actualEnd;
-
+    std::vector< std::vector< std::string > > _valueMatrix;
 };
 
 #endif /* end of include guard: SQLITERANGER_USA0FKBB */

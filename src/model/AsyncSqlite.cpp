dfb6139b013df45d03d6d67fd77b1f081d1011db
David <david@templatious.org>
2015-08-06 21:32:34 +0300

    dat namespace

--- src/model/AsyncSqlite.cpp
+++ src/model/AsyncSqlite.cpp
@@ -22,6 +22,39 @@ int snapshotBuilderCallback(void* snapshotBuilder,int argc,char** argv,char** co
 
 } // end of anon namespace
 
+namespace StackOverflow {
+
+    // http://stackoverflow.com/questions/4792449/c0x-has-no-semaphores-how-to-synchronize-threads
+    class Semaphore {
+    public:
+        Semaphore (int count = 0)
+            : _count(count) {}
+
+        inline void notify()
+        {
+            std::unique_lock<std::mutex> lock(_mtx);
+            ++_count;
+            _cv.notify_one();
+        }
+
+        inline void wait()
+        {
+            std::unique_lock<std::mutex> lock(_mtx);
+
+            while (_count == 0) {
+                _cv.wait(lock);
+            }
+            _count--;
+        }
+
+    private:
+        std::mutex _mtx;
+        std::condition_variable _cv;
+        int _count;
+    };
+
+}
+
 namespace SafeLists {
 
 struct AsyncSqliteImpl : public Messageable {

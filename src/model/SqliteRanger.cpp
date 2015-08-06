8bf1b3437cff98cb21d525c1180b33cde29eac76
David <david@templatious.org>
2015-08-06 18:44:01 +0300

    merr steff

--- src/model/SqliteRanger.cpp
+++ src/model/SqliteRanger.cpp
@@ -5,7 +5,11 @@ SqliteRanger::SqliteRanger(
     const std::weak_ptr< Messageable >& asyncSqlite,
     const UpdateFunction& updateFunction,
     const EmptyFunction& emptyFunction
-) : _asyncSqlite(asyncSqlite),
+) : _requestedStart(0),
+    _requestedEnd(0),
+    _actualStart(0),
+    _actualEnd(0),
+    _asyncSqlite(asyncSqlite),
     _updateFunction(updateFunction),
     _emptyValueFunction(emptyFunction)
 {

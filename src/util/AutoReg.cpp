281a0eea335139493312c0b1f42f8fad4efad2bf
David <david@templatious.org>
2015-08-23 09:18:07 +0300

    map prepped

--- src/util/AutoReg.cpp
+++ src/util/AutoReg.cpp
@@ -1,8 +1,22 @@
+#include <map>
+
 #include "AutoReg.hpp"
+
+namespace {
+
+typedef std::map<std::string,const templatious::TypeNode*> TNodeMap;
+
+static TNodeMap& getMap() {
+    static TNodeMap map;
+    return map;
+}
+
+}
 
 namespace SafeLists {
 
 static int registerTypeInMap(const char* name,const templatious::TypeNode* node) {
+    auto& m = getMap();
 
     return 7;
 }

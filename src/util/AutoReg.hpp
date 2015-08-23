1c019532059810a363fee7e8d5d3ce7db0192d44
David <david@templatious.org>
2015-08-23 11:35:40 +0300

    boom

--- src/util/AutoReg.hpp
+++ src/util/AutoReg.hpp
@@ -4,16 +4,25 @@
 #include <templatious/detail/DynamicPackCreator.hpp>
 
 #define DUMMY_REG(name,strname)  \
-    struct name { template <class Any> name(Any&&,int i = \
-        registerType()) {} name() {}    \
-        static int registerType() {     \
-            static int res = ::SafeLists::registerTypeInMap(strname,\
-                templatious::TypeNodeFactory::makeDummyNode< name >(strname));\
-            return res;\
-        }\
+    struct name { template <class Any> name(Any&&,int reg = Regger::s_sideEffect) {} \
+        typedef ::SafeLists::TypeRegger<name> Regger;                                \
+        name() {}                                                                    \
+        static int registerType() {                                                  \
+            static int res = ::SafeLists::registerTypeInMap(strname,                 \
+                templatious::TypeNodeFactory::makeDummyNode< name >(strname));       \
+            return res;                                                              \
+        }                                                                            \
     };
 
 namespace SafeLists {
+
+template <class T>
+struct TypeRegger {
+    static const int s_sideEffect;
+};
+
+template <class T>
+const int TypeRegger<T>::s_sideEffect = T::registerType();
 
 int registerTypeInMap(const char* name,const templatious::TypeNode* node);
 void traverseTypes(const std::function<void(const char*,const templatious::TypeNode*)>& func);

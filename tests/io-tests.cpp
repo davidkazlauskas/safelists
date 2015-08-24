f4b432286a0573e8c25acc54e431ac279bb5afac
David <david@templatious.org>
2015-08-24 18:29:33 +0300

    solid

--- tests/io-tests.cpp
+++ tests/io-tests.cpp
@@ -1,10 +1,18 @@
 
 #include <templatious/FullPack.hpp>
+#include <io/RandomFileWriter.hpp>
 
 #include "catch.hpp"
 
 TEMPLATIOUS_TRIPLET_STD;
 
 TEST_CASE("io_simple_read_write","[io]") {
+    SafeLists::RandomFileWriteHandle handle("some_file.txt",7);
+    handle.write("thetext",0,7);
+    char output[8];
+    handle.read(output,0,7);
+    output[7] = '\0';
 
+    std::string comp = output;
+    REQUIRE( comp == "thetext" );
 }

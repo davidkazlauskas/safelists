cffea09d63618b5aab0234a81e72df4f30755777
David <david@templatious.org>
2015-08-24 18:02:12 +0300

    should be FAIN

--- src/io/RandomFileWriter.cpp
+++ src/io/RandomFileWriter.cpp
@@ -1,12 +1,31 @@
 
+#include <fstream>
+
 #include "RandomFileWriter.hpp"
+
+namespace {
+
+    void fallocateFile(const char* path,int64_t size) {
+        // todo, optimize with fallocate for linux
+        std::ofstream file(path);
+        file.seekp(size - 1);
+        file.write("",1);
+    }
+
+}
 
 namespace SafeLists {
 
 RandomFileWriteHandle::RandomFileWriteHandle(const char* path,int64_t size)
     : _path(path), _size(size)
 {
+    // could be more optimized...
+    std::ifstream exists(_path.c_str());
+    if (!exists.is_open()) {
+        fallocateFile(_path.c_str(),_size);
+    }
 
+    _handle = fopen(_path.c_str(),"r+");
 }
 
 RandomFileWriteHandle::~RandomFileWriteHandle() {

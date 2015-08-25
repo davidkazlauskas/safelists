2cd54c8a9008a326e2380429db15c4ceb0bd9e71
David <david@templatious.org>
2015-08-25 18:46:08 +0300

    future test

--- tests/interval-tests.cpp
+++ tests/interval-tests.cpp
@@ -17,6 +17,24 @@ TEST_CASE("interval_throw_wrong_interval","[interval]") {
     REQUIRE( caught );
 }
 
+struct IntervalCollector {
+
+    bool operator()(const SafeLists::Interval& i) {
+        SA::add(_list,i);
+        return true;
+    }
+
+    std::vector< SafeLists::Interval > _list;
+};
+
 TEST_CASE("interval_list_append","[interval]") {
+
+    SafeLists::IntervalList list(0,1024);
+    IntervalCollector colEmpty;
+    IntervalCollector colFilled;
+
+    list.append(16,32);
+    list.traverseEmpty(colEmpty);
+    list.traverseFilled(colFilled);
 
 }

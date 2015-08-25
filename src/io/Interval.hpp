b9c13a03dbc5de296ac267ff322ced4263ed1839
David <david@templatious.org>
2015-08-25 18:08:08 +0300

    base

--- src/io/Interval.hpp
+++ src/io/Interval.hpp
@@ -2,6 +2,14 @@
 #ifndef INTERVAL_C5VNKZTP
 #define INTERVAL_C5VNKZTP
 
+namespace SafeLists {
 
+struct Interval {
+
+    int64_t _start;
+    int64_t _end;
+};
+
+}
 
 #endif /* end of include guard: INTERVAL_C5VNKZTP */

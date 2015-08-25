9e301d6cdd69a04049e73335ef32191e29dbbbba
David <david@templatious.org>
2015-08-25 18:42:27 +0300

    the test

--- tests/interval-tests.cpp
+++ tests/interval-tests.cpp
@@ -6,4 +6,13 @@
 
 TEMPLATIOUS_TRIPLET_STD;
 
+TEST_CASE("interval_throw_wrong_interval","[interval]") {
+    bool caught = false;
+    try {
+        SafeLists::Interval(2,1);
+    } catch (SafeLists::IntervalStartGreaterThanEndException&) {
+        caught = true;
+    }
 
+    REQUIRE( caught );
+}

5e5b88846cf3a8b5bfdc8dcbbcb8855343336757
David <david@templatious.org>
2015-08-27 20:21:18 +0300

    boom

--- src/io/AsyncDownloader.cpp
+++ src/io/AsyncDownloader.cpp
@@ -1,11 +1,14 @@
 
 #include <cstring>
+#include <future>
 
 #include "AsyncDownloader.hpp"
 
 namespace SafeLists {
 
     struct AsyncDownloaderImitationImpl : public Messageable {
+        AsyncDownloaderImitationImpl() : _shutdown(false) {}
+
         // this is for sending message across threads
         void message(const std::shared_ptr< templatious::VirtualPack >& msg) {
             _cache.enqueue(msg);
@@ -20,16 +23,33 @@ namespace SafeLists {
         static StrongMsgPtr spinupNew() {
             auto result = std::make_shared< AsyncDownloaderImitationImpl >();
 
+            std::thread(
+                [=]() {
+                    result->messageLoop();
+                }
+            ).detach();
+
             return result;
         }
 
     private:
+        void messageLoop() {
+            while (!_shutdown) {
+
+            }
+        }
+
+        void shutdown() {
+            _shutdown = true;
+        }
+
         MessageCache _cache;
+        bool _shutdown;
     };
 
     StrongMsgPtr AsyncDownloader::createNew(const char* type) {
         if (0 == strcmp(type,"imitation")) {
-            return std::make_shared< AsyncDownloaderImitationImpl >();
+            return AsyncDownloaderImitationImpl::spinupNew();
         }
         return nullptr;
     }

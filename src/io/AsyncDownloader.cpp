5bfab6064ab3d99cd234e701fafc6f05fe078df4
David <david@templatious.org>
2015-08-27 19:42:05 +0300

    carcass

--- src/io/AsyncDownloader.cpp
+++ src/io/AsyncDownloader.cpp
@@ -1,3 +1,14 @@
 
 #include "AsyncDownloader.hpp"
 
+namespace SafeLists {
+
+    struct AsyncDownloaderImpl : public Messageable {
+
+    };
+
+    StrongMsgPtr AsyncDownloader::createNew() {
+
+    }
+
+}
--- src/io/AsyncDownloader.hpp
+++ src/io/AsyncDownloader.hpp
@@ -1,7 +1,15 @@
 #ifndef ASYNCDOWNLOADER_7J8L0K2A
 #define ASYNCDOWNLOADER_7J8L0K2A
 
+#include <LuaPlumbing/plumbing.hpp>
+#include <util/DummyStruct.hpp>
+
 namespace SafeLists {
+
+    struct AsyncDownloader {
+
+        static StrongMsgPtr createNew();
+    };
 
 }
 

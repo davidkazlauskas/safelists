58a96c821333cc6b21ac5e32c1d45a4b062ad977
David <david@templatious.org>
2015-09-01 18:10:52 +0300

    new suite

---
+++ tests/downloader-tests.cpp
@@ -0,0 +1,11 @@
+
+#include <templatious/FullPack.hpp>
+#include <io/AsyncDownloader.hpp>
+
+#include "catch.hpp"
+
+TEMPLATIOUS_TRIPLET_STD;
+
+TEST_CASE("async_downloader_dummy","[async_downloader]") {
+}
+

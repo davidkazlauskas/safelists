0cdc39bb18895edd2dfb0c0d79731961140b24d9
David <david@templatious.org>
2015-09-01 18:28:32 +0300

    ensure

--- tests/downloader-tests.cpp
+++ tests/downloader-tests.cpp
@@ -6,6 +6,19 @@
 
 TEMPLATIOUS_TRIPLET_STD;
 
+namespace {
+    bool ensureExpected(int64_t size,const char* buffer) {
+        TEMPLATIOUS_0_TO_N(i,size) {
+            if (buffer[i] != '7') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
 TEST_CASE("async_downloader_dummy","[async_downloader]") {
+    auto downloader = SafeLists::AsyncDownloader::createNew("imitation");
+
 }
 

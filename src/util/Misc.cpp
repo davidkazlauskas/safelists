e82e1fb2d0dfe542ebd33ae1e20fd1f3e8976c0c
David <david@templatious.org>
2015-09-02 18:17:17 +0300

    solved the task of the century

--- src/util/Misc.cpp
+++ src/util/Misc.cpp
@@ -1,2 +1,11 @@
 #include "Misc.hpp"
 
+namespace SafeLists {
+
+bool isLittleEndianMachine() {
+    int16_t i16 = 7;
+    char* reint = reinterpret_cast<char*>(&i16);
+    return reint[0] == 7;
+}
+
+}

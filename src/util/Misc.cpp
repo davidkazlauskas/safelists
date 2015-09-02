b385c254dd4264e2970b591d775e48c30355097a
David <david@templatious.org>
2015-09-02 18:23:07 +0300

    write func

--- src/util/Misc.cpp
+++ src/util/Misc.cpp
@@ -8,4 +8,9 @@ bool isLittleEndianMachine() {
     return reint[0] == 7;
 }
 
+void writeI64AsLittleEndian(int64_t number,char array[sizeof(number)]) {
+    static bool isLittle = isLittleEndianMachine();
+    static_assert( sizeof(number) == 8, "I'm so paranoid it's not even funny." );
+}
+
 }

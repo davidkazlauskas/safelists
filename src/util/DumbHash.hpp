02526df15f0597cc5b0cfd012e80476be0850ea2
David <david@templatious.org>
2015-09-03 20:15:50 +0300

    dumb start

--- src/util/DumbHash.hpp
+++ src/util/DumbHash.hpp
@@ -4,6 +4,17 @@
 
 namespace SafeLists {
 
+    // the only advantage
+    // of this hash vs sha256
+    // and stuff like that
+    // is that you can sum bytes
+    // in any order and the result will be the same;
+    struct DumbHash256 {
+
+    private:
+        char _mem[32];
+    };
+
 }
 
 #endif /* end of include guard: DUMBHASH_DGGM3OP4 */

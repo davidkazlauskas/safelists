b5aff58d13be493b8c24f8c70aa88862ec76ed2f
David <david@templatious.org>
2015-09-03 19:42:05 +0300

    first impl

--- src/io/SafeListDownloader.cpp
+++ src/io/SafeListDownloader.cpp
@@ -3,5 +3,16 @@
 
 namespace SafeLists {
 
+StrongMsgPtr SafeListDownloader::startNew(
+        const char* path,
+        const StrongMsgPtr& fileWriter,
+        const StrongMsgPtr& fileDownloader,
+        const WeakMsgPtr& toNotify
+)
+{
+
+    return nullptr;
+}
+
 }
 
--- src/io/SafeListDownloader.hpp
+++ src/io/SafeListDownloader.hpp
@@ -1,7 +1,21 @@
 #ifndef SAFELISTDOWNLOADER_XE646FNT
 #define SAFELISTDOWNLOADER_XE646FNT
 
+#include <LuaPlumbing/plumbing.hpp>
+#include <util/AutoReg.hpp>
+
 namespace SafeLists {
+
+struct SafeListDownloader {
+
+
+    static StrongMsgPtr startNew(
+            const char* path,
+            const StrongMsgPtr& fileWriter,
+            const StrongMsgPtr& fileDownloader,
+            const WeakMsgPtr& toNotify
+    );
+};
 
 }
 

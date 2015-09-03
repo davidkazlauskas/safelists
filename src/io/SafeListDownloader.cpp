acba16395f3913837f8cee92137c7801188525b0
David <david@templatious.org>
2015-09-03 19:45:37 +0300

    bam

--- src/io/SafeListDownloader.cpp
+++ src/io/SafeListDownloader.cpp
@@ -3,6 +3,16 @@
 
 namespace SafeLists {
 
+struct SafeListDownloaderImpl : public Messageable {
+    void message(const std::shared_ptr< templatious::VirtualPack >& msg) override {
+        // do stuff
+    }
+
+    void message(templatious::VirtualPack& msg) override {
+        assert( false && "Synchronous messages disabled on SafeListDownloader." );
+    }
+};
+
 StrongMsgPtr SafeListDownloader::startNew(
         const char* path,
         const StrongMsgPtr& fileWriter,

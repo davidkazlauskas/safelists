290a7b0e6a8a3902d4b03ca157b9731a30a787a8
David <david@templatious.org>
2015-09-04 16:26:02 +0300

    dumb the, the main test forgot of course

--- tests/util-tests.cpp
+++ tests/util-tests.cpp
@@ -87,3 +87,32 @@ TEST_CASE("dumb_hash_basic_append_midsplit","[util]") {
 
     REQUIRE( buf == expected );
 }
+
+TEST_CASE("dumb_hash_the_cake","[util]") {
+    std::mt19937 tw(7);
+    std::vector< char > toApply;
+    TEMPLATIOUS_REPEAT( 256 * 2 ) {
+        SA::add(toApply,tw());
+    }
+
+    SafeLists::DumbHash256 hashA;
+    SafeLists::DumbHash256 hashB;
+    auto sf = SF::seqL(256*2);
+    TEMPLATIOUS_FOREACH(auto i,sf) {
+        hashA.add(i,toApply[i]);
+    }
+
+    auto sr = sf.rev();
+    TEMPLATIOUS_FOREACH(auto i,sr) {
+        hashB.add(i,toApply[i]);
+    }
+
+    char strA[65];
+    char strB[65];
+    hashA.toString(strA);
+    hashB.toString(strB);
+
+    std::string aStr = strA;
+    std::string bStr = strB;
+    REQUIRE( aStr == bStr );
+}

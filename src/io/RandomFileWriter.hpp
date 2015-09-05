488264d337c31bf54b33d848f260e0844b76360e
David <david@templatious.org>
2015-09-05 11:33:12 +0300

    first interface

--- src/io/RandomFileWriter.hpp
+++ src/io/RandomFileWriter.hpp
@@ -7,6 +7,17 @@ namespace SafeLists {
 
 struct RandomFileWriter {
 
+    // write block into file.
+    // Signature:
+    // <
+    //    WriteData,
+    //    std::string (filename),
+    //    std::unique_ptr< char[] > (data),
+    //    int64_t (start),
+    //    int64_t (end)
+    // >
+    DUMMY_REG(WriteData,"RFW_WriteData");
+
     // singleton
     static StrongMsgPtr make();
 };

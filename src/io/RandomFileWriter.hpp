72192e9539ac4bf71edc4dc91c983e87d2f0d68e
David <david@templatious.org>
2015-09-05 11:27:07 +0300

    new sources

---
+++ src/io/RandomFileWriter.hpp
@@ -0,0 +1,13 @@
+#ifndef RANDOMFILEWRITER_7DHBRNML
+#define RANDOMFILEWRITER_7DHBRNML
+
+namespace SafeLists {
+
+struct RandomFileWriter {
+
+
+};
+
+}
+
+#endif /* end of include guard: RANDOMFILEWRITER_7DHBRNML */

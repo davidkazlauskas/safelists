74b0a88df3dd7693d0fb3fc31f5d51d229cb0502
David <david@templatious.org>
2015-09-05 12:35:31 +0300

    carcass

--- src/io/SafeListDownloader.cpp
+++ src/io/SafeListDownloader.cpp
@@ -5,13 +5,26 @@
 namespace SafeLists {
 
 struct SafeListDownloaderImpl : public Messageable {
+    SafeListDownloaderImpl() = delete;
+    SafeListDownloaderImpl(const SafeListDownloaderImpl&) = delete;
+    SafeListDownloaderImpl(SafeListDownloaderImpl&&) = delete;
+
     void message(const std::shared_ptr< templatious::VirtualPack >& msg) override {
-        // do stuff
+        _cache.enqueue(msg);
     }
 
     void message(templatious::VirtualPack& msg) override {
         assert( false && "Synchronous messages disabled on SafeListDownloader." );
     }
+
+    static std::shared_ptr< SafeListDownloaderImpl > startSession(
+            const char* path)
+    {
+        return nullptr;
+    }
+private:
+    std::string _path;
+    MessageCache _cache;
 };
 
 StrongMsgPtr SafeListDownloader::startNew(

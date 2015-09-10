9d2538e63aa568ab27508b66b6c703347b0fa9b1
David <david@templatious.org>
2015-09-10 18:17:21 +0300

    carcass

--- src/gtkmm/GtkMMSessionWidget.hpp
+++ src/gtkmm/GtkMMSessionWidget.hpp
@@ -1,7 +1,15 @@
 #ifndef GTKMMSESSIONWIDGET_SD2Z0QLJ
 #define GTKMMSESSIONWIDGET_SD2Z0QLJ
 
+#include <gtkmm.h>
+
 namespace SafeLists {
+
+struct GtkSessionWidget {
+    GtkSessionWidget(Glib::RefPtr<Gtk::Builder>& bld) {
+
+    }
+};
 
 }
 

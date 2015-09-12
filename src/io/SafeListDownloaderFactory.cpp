886bed957f0998d264bf65133d14323bfc650e39
David <david@templatious.org>
2015-09-12 10:45:52 +0300

    out msg

--- src/io/SafeListDownloaderFactory.cpp
+++ src/io/SafeListDownloaderFactory.cpp
@@ -1,9 +1,36 @@
+
+#include <templatious/FullPack.hpp>
+
 #include "SafeListDownloaderFactory.hpp"
+
+TEMPLATIOUS_TRIPLET_STD;
 
 namespace SafeLists {
 
 struct SafeListDownloaderFactoryImpl : public Messageable {
 
+private:
+    VmfPtr genHandler() {
+        typedef SafeListDownloaderFactory SLDF;
+        return SF::virtualMatchFunctorPtr(
+            SF::virtualMatch<
+                SLDF::InNewAsync,
+                std::string, // path
+                StrongMsgPtr, // to notify
+                StrongMsgPtr // out object
+            >(
+                [=](SLDF::InNewAsync,
+                    const std::string& path,
+                    const StrongMsgPtr& toNotify,
+                    StrongMsgPtr& output)
+                {
+
+                }
+            )
+        );
+    }
+
+    VmfPtr _handler;
 };
 
 StrongMsgPtr SafeListDownloaderFactory::createNew() {

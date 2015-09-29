f3e94d9747d714d096d888cde09d4775cc2b717e
David <david@templatious.org>
2015-09-29 10:14:59 +0300

    new header

---
+++ src/util/GracefulShutdownGuard.hpp
@@ -0,0 +1,13 @@
+
+#ifndef GRACEFULSHUTDOWNGUARD_AAF605J2
+#define GRACEFULSHUTDOWNGUARD_AAF605J2
+
+namespace SafeLists {
+
+    struct GracefulShutdownGuard {
+    };
+
+}
+
+#endif /* end of include guard: GRACEFULSHUTDOWNGUARD_AAF605J2 */
+

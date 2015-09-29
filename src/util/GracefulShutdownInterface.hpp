b027edf2662380dfcb18e31f4e2b1ff172568fb7
David <david@templatious.org>
2015-09-29 11:15:06 +0300

    some docs

--- src/util/GracefulShutdownInterface.hpp
+++ src/util/GracefulShutdownInterface.hpp
@@ -1,10 +1,19 @@
 #ifndef GRACEFULSHUTDOWNINTERFACE_XMC7PIDI
 #define GRACEFULSHUTDOWNINTERFACE_XMC7PIDI
 
-#include <util/AutoReg.hpp>
+#include <util/DummyStruct.hpp>
 
 namespace SafeLists {
     struct GracefulShutdownInterface {
+        // Send this to messageable object
+        // with the handle to the messageable.
+        // Signature: < InRegisterItself, StrongMsgPtr >
+        DUMMY_STRUCT(InRegisterItself);
+
+        // Send this from messageable object
+        // with new messageable to be put into queue
+        // Signature: < OutRegisterItself, StrongMsgPtr >
+        DUMMY_STRUCT(OutRegisterItself);
     };
 }
 

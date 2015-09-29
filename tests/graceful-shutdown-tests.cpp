806b67fe157191fd84c76235b8c2bb9f6f47043b
David <david@templatious.org>
2015-09-29 14:02:34 +0300

    shutdown class carcass

--- tests/graceful-shutdown-tests.cpp
+++ tests/graceful-shutdown-tests.cpp
@@ -6,6 +6,25 @@
 
 TEMPLATIOUS_TRIPLET_STD;
 
+struct Sleep100MsShutdownClass : public Messageable {
+
+    Sleep100MsShutdownClass(const Sleep100MsShutdownClass&) = delete;
+    Sleep100MsShutdownClass(Sleep100MsShutdownClass&&) = delete;
+
+    void message(const std::shared_ptr< templatious::VirtualPack >& msg) override {
+        _cache.enqueue(msg);
+    }
+
+    void message(templatious::VirtualPack& msg) override {
+        assert( false && "..." );
+    }
+
+private:
+    Sleep100MsShutdownClass();
+
+    MessageCache _cache;
+};
+
 TEST_CASE("graceful_shutdown_guard_test","[graceful_shutdown]") {
     using namespace SafeLists;
 

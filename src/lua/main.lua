705b0a0dad85f6e7fb684933fcf6b8d5817ee99c
David <david@templatious.org>
2015-10-01 17:43:08 +0300

    just redesign it

--- src/lua/main.lua
+++ src/lua/main.lua
@@ -420,7 +420,7 @@ initAll = function()
             VString(message))
     end
 
-    local newFileDialog = function(funcSuccess,funcDialogTweak)
+    local newFileDialog = function(funcSuccess)
         local dialogService = ctx:namedMessageable("dialogService")
 
         -- Return results:
@@ -507,10 +507,6 @@ initAll = function()
             dialog,
             VSig("INDLG_InAlwaysAbove")
         )
-
-        if (funcDialogTweak) then
-            funcDialogTweak(dialog)
-        end
 
         ctx:message(
             dialog,

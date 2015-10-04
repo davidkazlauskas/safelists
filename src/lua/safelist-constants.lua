073ae189a7be516d2f0fcae3b15086f1a00c41db
David <david@templatious.org>
2015-10-04 13:15:34 +0300

    meren

--- src/lua/safelist-constants.lua
+++ src/lua/safelist-constants.lua
@@ -5,8 +5,8 @@ CREATE TABLE directories (dir_id INTEGER PRIMARY KEY AUTOINCREMENT,dir_name TEXT
 CREATE TABLE files (file_id INTEGER PRIMARY KEY AUTOINCREMENT,dir_id INT,file_name TEXT NOT NULL,file_size INT,file_hash_256 TEXT);
 CREATE TABLE mirrors (file_id INTEGER,url TEXT,use_count INT);
 CREATE TABLE metadata (revision_number INT,modification_date INT,safelist_version INT);
-CREATE TRIGGER after_insert_files AFTER INSERT ON files
 INSERT INTO metadata (revision_number,modification_date,safelist_version) VALUES(0,0,100);
+CREATE TRIGGER after_insert_files AFTER INSERT ON files
 BEGIN
     UPDATE metadata
     SET revision_number=revision_number+1,modification_date=strftime('%s','now');

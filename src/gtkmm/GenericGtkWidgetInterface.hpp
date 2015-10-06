6f0721b3f114f7690743458ed55d814e3cb9c315
David <david@templatious.org>
2015-10-06 18:14:51 +0300

    new header

---
+++ src/gtkmm/GenericGtkWidgetInterface.hpp
@@ -0,0 +1,10 @@
+#ifndef GENERICGTKWIDGETINTERFACE_MDS06G4N
+#define GENERICGTKWIDGETINTERFACE_MDS06G4N
+
+#include <util/AutoReg.hpp>
+
+struct GenericGtkWidgetInterface {
+    DUMMY_REG(GetWidgetFromTree,"GWI_GetWidgetFromTree");
+};
+
+#endif /* end of include guard: GENERICGTKWIDGETINTERFACE_MDS06G4N */

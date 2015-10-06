da6d0e09ee624eb2382db0c2e91ad2e0dc7c808d
David <david@templatious.org>
2015-10-06 18:20:54 +0300

    now that is what I call interface

--- src/gtkmm/GenericGtkWidgetInterface.hpp
+++ src/gtkmm/GenericGtkWidgetInterface.hpp
@@ -10,11 +10,25 @@ struct GenericGtkWidgetInterface {
     DUMMY_REG(GetWidgetFromTree,"GWI_GetWidgetFromTree");
 };
 
+struct GenericWidgetTrait {
+    // Set wether widget is active.
+    // Signature:
+    // < SetActive, bool (value) >
+    DUMMY_REG(SetActive,"GWI_GWT_SetActive");
+};
+
 struct GenericLabelTrait {
     // Set label value.
     // Signature:
-    // < GetWidgetFromTree, std::string (name), StrongMsgPtr (out) >
+    // < SetValue, std::string (value) >
     DUMMY_REG(SetValue,"GWI_GLT_SetValue");
+};
+
+struct GenericInputTrait {
+    // Query input value (as string)
+    // Signature:
+    // < QueryValue, std::string (out value) >
+    DUMMY_REG(QueryValue,"GWI_GIT_QueryValue");
 };
 
 #endif /* end of include guard: GENERICGTKWIDGETINTERFACE_MDS06G4N */

a21c73923b223c0a96bba5e26857c54fff4ca295
David <david@templatious.org>
2015-10-06 18:17:12 +0300

    label trait

--- src/gtkmm/GenericGtkWidgetInterface.hpp
+++ src/gtkmm/GenericGtkWidgetInterface.hpp
@@ -10,4 +10,11 @@ struct GenericGtkWidgetInterface {
     DUMMY_REG(GetWidgetFromTree,"GWI_GetWidgetFromTree");
 };
 
+struct GenericLabelTrait {
+    // Set label value.
+    // Signature:
+    // < GetWidgetFromTree, std::string (name), StrongMsgPtr (out) >
+    DUMMY_REG(SetValue,"GWI_GLT_SetValue");
+};
+
 #endif /* end of include guard: GENERICGTKWIDGETINTERFACE_MDS06G4N */

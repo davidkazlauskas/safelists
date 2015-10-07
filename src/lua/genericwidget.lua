cf1995666008e96a4c3422fe8959f18b6c3303f0
David <david@templatious.org>
2015-10-07 18:16:27 +0300

    generic widget get

--- src/lua/genericwidget.lua
+++ src/lua/genericwidget.lua
@@ -1,14 +1,48 @@
 
 GenericWidget = {
     __index = {
-        putOn = function(strongMsg)
+        putOn = function(strongMsg,context)
+            if (nil == context) then
+                context = luaContext()
+            end
             local res = {
                 messageable = strongMsg,
-                nodeCache = {}
+                nodeCache = {},
+                luaCtx = context
             }
             setmetatable(res,GenericWidget)
             return res
+        end,
+        getWidget = function(self,name)
+            assert( type(name) == "string",
+                "Expected string for name." )
+
+            local res = self.nodeCache[name]
+            if (nil == res) then
+                local msg = self.luaCtx:messageRetValues(
+                    VSig("GWI_GetWidgetFromTree"),
+                    VString(name),
+                    VMsg(nil)
+                )._3
+
+                res = {
+                    messageable = msg,
+                    luaCtx = self.context
+                }
+
+                setmetatable(res,GenericWidgetNode)
+
+                self.nodeCache[name] = res
+            end
+
+            return res
         end
+    }
+}
+
+GenericWidgetNode = {
+    __index = {
+        -- todo
     }
 }
 

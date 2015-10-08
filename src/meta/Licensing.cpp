713389e6bf898584f89e101c1f71ebcf4eb50e11
David <david@templatious.org>
2015-10-08 18:06:36 +0300

    basic impl

--- src/meta/Licensing.cpp
+++ src/meta/Licensing.cpp
@@ -1,7 +1,25 @@
 
+#include <cassert>
 #include "Licensing.hpp"
 
 namespace SafeLists {
+
+struct LicenseDaemonDummyImpl : public Messageable {
+
+    // this is for sending message across threads
+    void message(const std::shared_ptr< templatious::VirtualPack >& msg) override {
+        _cache.enqueue(msg);
+    }
+
+    // this is for sending stack allocated (faster)
+    // if we know we're on the same thread as GUI
+    void message(templatious::VirtualPack& msg) override {
+        assert( false && "Only async messages in license daemon." );
+    }
+
+private:
+    MessageCache _cache;
+};
 
 StrongMsgPtr LicenseDaemon::getDaemon() {
     static StrongMsgPtr theObject = nullptr;

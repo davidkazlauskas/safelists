31a3c18ef28c7e41ad80a635087b238f3770069f
David <david@templatious.org>
2015-10-08 18:14:11 +0300

    main loop

--- src/meta/Licensing.cpp
+++ src/meta/Licensing.cpp
@@ -1,14 +1,21 @@
 
 #include <cassert>
+#include <util/Semaphore.hpp>
 #include "Licensing.hpp"
+
+TEMPLATIOUS_TRIPLET_STD;
 
 namespace SafeLists {
 
 struct LicenseDaemonDummyImpl : public Messageable {
+    LicenseDaemonDummyImpl() : _handler(genHandler()) {}
+    LicenseDaemonDummyImpl(const LicenseDaemonDummyImpl&) = delete;
+    LicenseDaemonDummyImpl(LicenseDaemonDummyImpl&&) = delete;
 
     // this is for sending message across threads
     void message(const std::shared_ptr< templatious::VirtualPack >& msg) override {
         _cache.enqueue(msg);
+        _sem.notify();
     }
 
     // this is for sending stack allocated (faster)
@@ -17,8 +24,33 @@ struct LicenseDaemonDummyImpl : public Messageable {
         assert( false && "Only async messages in license daemon." );
     }
 
+    void mainLoop() {
+        while (_keepGoing) {
+            _sem.wait();
+            _cache.process(
+                [=](templatious::VirtualPack& pack) {
+                    _handler->tryMatch(pack);
+                }
+            );
+        }
+    }
 private:
+    VmfPtr genHandler() {
+        typedef LicenseDaemon LD;
+        return SF::virtualMatchFunctorPtr(
+            SF::virtualMatch< LD::IsExpired, bool >(
+                [=](ANY_CONV,bool& res) {
+                    // do something, read files or whatever
+                    return res = true;
+                }
+            )
+        );
+    }
+
     MessageCache _cache;
+    StackOverflow::Semaphore _sem;
+    VmfPtr _handler;
+    bool _keepGoing;
 };
 
 StrongMsgPtr LicenseDaemon::getDaemon() {

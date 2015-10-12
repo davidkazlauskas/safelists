615372cc5ea55aa21e0b1e693fa6217d68082322
David <david@templatious.org>
2015-10-12 18:12:57 +0300

    the sig

--- src/meta/SignatureCheck.hpp
+++ src/meta/SignatureCheck.hpp
@@ -1,6 +1,13 @@
 #ifndef SIGNATURECHECK_DEV9MTG8
 #define SIGNATURECHECK_DEV9MTG8
 
+#include <string>
+#include <vector>
 
+namespace SafeLists {
+
+    void hashFileListSha256(const std::vector<std::string>& paths);
+
+}
 
 #endif /* end of include guard: SIGNATURECHECK_DEV9MTG8 */

3a8fa8bb784d7773d3c978e656485bd193dfc555
David <david@templatious.org>
2015-10-12 19:22:08 +0300

    should werk

--- src/meta/SignatureCheck.cpp
+++ src/meta/SignatureCheck.cpp
@@ -1,8 +1,11 @@
 
 #include <fstream>
+#include <templatious/FullPack.hpp>
 #include <crypto++/sha.h>
 
 #include "SignatureCheck.hpp"
+
+TEMPLATIOUS_TRIPLET_STD;
 
 namespace {
 
@@ -26,6 +29,14 @@ namespace {
         return true;
     }
 
+    void bytesToCStr(char* raw,char* string,int rawNum) {
+        TEMPLATIOUS_0_TO_N(i,rawNum) {
+            char* strPtr = string + i * 2;
+            char toConv = raw[i];
+            sprintf(strPtr,"%02x",toConv);
+        }
+    }
+
 }
 
 namespace SafeLists {
@@ -33,7 +44,19 @@ namespace SafeLists {
 std::string hashFileListSha256(const std::vector<std::string>& paths) {
     CryptoPP::SHA256 hash;
 
-    return "";
+    TEMPLATIOUS_FOREACH(auto& i, paths) {
+        bool res = hashSingleFile(hash,i);
+        if (!res) {
+            return "";
+        }
+    }
+
+    char bytes[1024];
+    char bytesStr[2048];
+
+    hash.Final(reinterpret_cast<unsigned char*>(bytes));
+    bytesToCStr(bytes,bytesStr,sizeof(bytes));
+    return bytesStr;
 }
 
 }

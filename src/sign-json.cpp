587be800488f488a03aadb96ff045af105e3a323
David <david@templatious.org>
2015-10-14 17:30:22 +0300

    first errors

--- src/sign-json.cpp
+++ src/sign-json.cpp
@@ -2,8 +2,22 @@
 #include <meta/SignatureCheck.hpp>
 
 int main(int argc,char* argv[]) {
-    if (argc != 3) {
+    if (argc != 2) {
         printf("Usage: signjson <private key path> <signature file>\n");
+        return 1;
+    }
+
+    std::vector< std::string > paths;
+    std::string outSig;
+    auto res = SafeLists::getFileList(argv[1],paths,outSig);
+    if (res != SafeLists::GetFileListError::Success) {
+        if (res == SafeLists::GetFileListError::InvalidJson) {
+            printf("Invalid json...\n");
+        } else if (res == SafeLists::GetFileListError::CouldNotOpenFile) {
+            printf("Could not open file...\n");
+        } else if (res == SafeLists::GetFileListError::ParseError) {
+            printf("Json parsing error...\n");
+        }
         return 1;
     }
 }

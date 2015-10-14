fc81a20e746cb6d405f50c92f78d1f49c43da919
David <david@templatious.org>
2015-10-14 17:27:12 +0300

    usage

--- src/sign-json.cpp
+++ src/sign-json.cpp
@@ -1,7 +1,10 @@
 
-
+#include <meta/SignatureCheck.hpp>
 
 int main(int argc,char* argv[]) {
-
+    if (argc != 3) {
+        printf("Usage: signjson <private key path> <signature file>\n");
+        return 1;
+    }
 }
 

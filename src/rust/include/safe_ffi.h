f0eabc2a40c8d7d8e48e41e76f073d0fc134c318
David <david@templatious.org>
2015-10-21 19:06:40 +0300

    first function

--- src/rust/include/safe_ffi.h
+++ src/rust/include/safe_ffi.h
@@ -1,6 +1,12 @@
 #ifndef SAFE_FFI_BDET7AES
 #define SAFE_FFI_BDET7AES
 
+#include <cstdint>
 
+extern "C" {
+
+    int32_t create_unregistered_client(void** out);
+
+}
 
 #endif /* end of include guard: SAFE_FFI_BDET7AES */

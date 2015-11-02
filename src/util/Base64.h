e4c77524eaa09dd348ee3879b6b1b52f5d2ff8c1
David <david@templatious.org>
2015-11-02 17:35:31 +0200

    decode

--- src/util/Base64.h
+++ src/util/Base64.h
@@ -4,5 +4,6 @@
 #include <inttypes.h>
 
 int base64encode(const void* data_buf, size_t dataLength, char* result, size_t resultSize);
+int base64decode(char *in, size_t inLen, unsigned char *out, size_t *outLen);
 
 #endif /* end of include guard: BASE64_Q11II03Q */

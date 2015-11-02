415117d26e26d60a273b56e9807ec7dc35805c54
David <david@templatious.org>
2015-11-02 17:33:13 +0200

    base64 copied impl

---
+++ src/util/Base64.h
@@ -0,0 +1,8 @@
+#ifndef BASE64_Q11II03Q
+#define BASE64_Q11II03Q
+
+#include <inttypes.h>
+
+int base64encode(const void* data_buf, size_t dataLength, char* result, size_t resultSize);
+
+#endif /* end of include guard: BASE64_Q11II03Q */

9655d29ad7027b58be090d7c7f0cabcdd569ca01
David <david@templatious.org>
2015-11-21 12:43:41 +0200

    new header

---
+++ src/rust/include/safe_file_downloader.h
@@ -0,0 +1,12 @@
+#ifndef SAFE_FILE_DOWNLOADER_JD3CE0PO
+#define SAFE_FILE_DOWNLOADER_JD3CE0PO
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* end of include guard: SAFE_FILE_DOWNLOADER_JD3CE0PO */

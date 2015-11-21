e3595bea49fc9609a8e3985edf9ac565f12e9cce
David <david@templatious.org>
2015-11-21 11:18:35 +0200

    first stab

--- src/rust/safe_file_downloader/src/lib.rs
+++ src/rust/safe_file_downloader/src/lib.rs
@@ -1,3 +1,19 @@
+extern crate libc;
+
+struct DownloadTask {
+    userdata: *mut libc::c_void,
+    userdata_buffer_func: extern fn(
+        param: *mut libc::c_void,
+        libc::int64_t,
+        libc::int64_t,
+        *mut libc::c_char),
+    userdata_destructor: extern fn(param: *mut libc::c_void),
+}
+
+struct DownloaderActor {
+    tasks: Vec< DownloadTask >,
+}
+
 #[test]
 fn it_works() {
 }

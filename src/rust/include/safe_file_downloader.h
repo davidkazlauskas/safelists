b8183c5b925d418dfa110360dbf28e6f9f6e93db
David <david@templatious.org>
2015-11-21 12:45:43 +0200

    the header

--- src/rust/include/safe_file_downloader.h
+++ src/rust/include/safe_file_downloader.h
@@ -1,9 +1,14 @@
 #ifndef SAFE_FILE_DOWNLOADER_JD3CE0PO
 #define SAFE_FILE_DOWNLOADER_JD3CE0PO
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+    void* safe_file_downloader_new();
+    void safe_file_downloader_cleanup(void* handle);
 
 #ifdef __cplusplus
 }

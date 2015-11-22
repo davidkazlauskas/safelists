0bdd3a27605a05bcbc76b26416a97997a38be965
David <david@templatious.org>
2015-11-22 20:23:10 +0200

    userdata representation

--- src/rust/include/safe_file_downloader.h
+++ src/rust/include/safe_file_downloader.h
@@ -6,6 +6,22 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+    typedef int32_t (*sfd_userdata_buffer_func)(
+        void* userdata,int64_t start,int64_t end,const uint8_t* buf);
+
+    typedef void (*sfd_userdata_arbitrary_message_func)(
+        void* userdata,int32_t msgtype,const void* buf);
+
+    typedef void (*sfd_userdata_destructor)(
+        void* userdata);
+
+    struct safe_file_downloader_args {
+        void* userdata;
+        sfd_userdata_buffer_func userdata_buffer_func;
+        sfd_userdata_arbitrary_message_func userdata_arbitrary_message_func;
+        sfd_userdata_destructor userdata_destructor;
+        const char* path;
+    };
 
     void* safe_file_downloader_new();
     void safe_file_downloader_cleanup(void* handle);

ecc4619f4d0bb96a6fc91a970d924bcd9e11ba38
David <david@templatious.org>
2015-11-22 20:19:04 +0200

    return if we want to continue

--- src/rust/safe_file_downloader/src/lib.rs
+++ src/rust/safe_file_downloader/src/lib.rs
@@ -94,7 +94,7 @@ struct DownloadTask {
         param: *mut libc::c_void,
         libc::uint64_t,
         libc::uint64_t,
-        *const libc::uint8_t),
+        *const libc::uint8_t) -> libc::int32_t,
     userdata_arbitrary_message_func: extern fn(
         param: *mut libc::c_void,
         msgtype: i32,
@@ -401,7 +401,7 @@ struct ScheduleArgs {
         param: *mut libc::c_void,
         libc::uint64_t,
         libc::uint64_t,
-        *const libc::uint8_t),
+        *const libc::uint8_t) -> libc::int32_t,
     userdata_arbitrary_message_func: extern fn(
         param: *mut libc::c_void,
         msgtype: i32,

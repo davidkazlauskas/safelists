c9b590ee3486752f77a0ad1b29ad196d13731c8e
David <david@templatious.org>
2015-11-23 18:59:57 +0200

    api fixes

--- src/io/AsyncDownloader.hpp
+++ src/io/AsyncDownloader.hpp
@@ -14,6 +14,7 @@ namespace SafeLists {
         // Schedule.
         // Signature: <
         //     ScheduleDownload,
+        //     const std::string, // path
         //     Interval, // file interval
         //     std::function< bool(const char*,int64_t,int64_t) >, // byte process function
         //     std::weak_ptr< Messageable > // handle other misc errors
@@ -21,6 +22,7 @@ namespace SafeLists {
         //
         // Signature: <
         //     ScheduleDownload,
+        //     const std::string, // path
         //     IntervalList, // file interval, for unfinished, partially downloaded files
         //     std::function< bool(const char*,int64_t,int64_t) >, // byte process function
         //     std::weak_ptr< Messageable > // handle other misc errors
@@ -30,6 +32,10 @@ namespace SafeLists {
         // Signalled when download is finished.
         // Signature: < OutDownloadFinished >
         DUMMY_REG(OutDownloadFinished,"AD_OutDownloadFinished");
+
+        // Signalled when file is not found.
+        // Signature: < OutDownloadFinished >
+        DUMMY_REG(OutFileNotFound,"AD_OutFileNotFound");
 
         static StrongMsgPtr createNew(const char* type = "imitation");
     };

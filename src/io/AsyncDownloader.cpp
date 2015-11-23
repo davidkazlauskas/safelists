b498a5c7009b9c1fb59914f20b045ea830b8bb3d
David <david@templatious.org>
2015-11-23 18:23:01 +0200

    them strings...

--- src/io/AsyncDownloader.cpp
+++ src/io/AsyncDownloader.cpp
@@ -332,11 +332,13 @@ namespace SafeLists {
             return SF::virtualMatchFunctorPtr(
                 SF::virtualMatch<
                     AD::ScheduleDownload,
+                    const std::string,
                     IntervalList,
                     ByteFunction,
                     std::weak_ptr< Messageable >
                 >(
                     [=](AD::ScheduleDownload,
+                        const std::string& url,
                         IntervalList& interval,
                         const ByteFunction& func,
                         const std::weak_ptr< Messageable >& wmsg)
@@ -355,11 +357,13 @@ namespace SafeLists {
                 ),
                 SF::virtualMatch<
                     AD::ScheduleDownload,
+                    const std::string,
                     Interval,
                     ByteFunction,
                     std::weak_ptr< Messageable >
                 >(
                     [=](AD::ScheduleDownload,
+                        const std::string& url,
                         const Interval& interval,
                         const ByteFunction& func,
                         const std::weak_ptr< Messageable >& wmsg)

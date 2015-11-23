3e48061337ed8887dd65ac2a4660e38346b91e5f
David <david@templatious.org>
2015-11-23 17:27:47 +0200

    undone

--- src/rust/include/safe_file_downloader.h
+++ src/rust/include/safe_file_downloader.h
@@ -24,7 +24,7 @@ extern "C" {
     };
 
     void* safe_file_downloader_new();
-    void safe_file_downloader_cleanup(void* handle);
+    void safe_file_downloader_cleanup(void* handle,sfd_userdata_function on_done,void* on_done_data);
     void safe_file_downloader_schedule(void* handle,void* args /* safe_file_downloader_args */ );
 
 #ifdef __cplusplus

27cf7ab5347e5dd95378dceb5118287dd23c0853
David <david@templatious.org>
2015-11-23 17:24:12 +0200

    typedef change

--- src/rust/include/safe_file_downloader.h
+++ src/rust/include/safe_file_downloader.h
@@ -12,14 +12,14 @@ extern "C" {
     typedef void (*sfd_userdata_arbitrary_message_func)(
         void* userdata,int32_t msgtype,const void* buf);
 
-    typedef void (*sfd_userdata_destructor)(
+    typedef void (*sfd_userdata_function)(
         void* userdata);
 
     struct safe_file_downloader_args {
         void* userdata;
         sfd_userdata_buffer_func userdata_buffer_func;
         sfd_userdata_arbitrary_message_func userdata_arbitrary_message_func;
-        sfd_userdata_destructor userdata_destructor;
+        sfd_userdata_function userdata_destructor;
         const char* path;
     };
 

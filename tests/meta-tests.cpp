10b038e7190b311ec0f42436e12abd920a012eb7
David <david@templatious.org>
2015-12-15 18:35:23 +0200

    sodium ftw

--- tests/meta-tests.cpp
+++ tests/meta-tests.cpp
@@ -63,13 +63,8 @@ TEST_CASE("sign_files","[meta]") {
     REQUIRE( resVer == SafeLists::VerifyFileListError::Success );
 
     const char* EXPECTED =
-      "138016af591e7b87798457d1eb60423c55de96438ecee23107c38b448b9a608782075cf5d64"
-      "520b47f74b0e820badf985a5b736815da208dc865e149803d94a97484e525d5417387500460"
-      "e67f36232c91da32a1ab3c83390b4508290344ee062c11dda1b8d76b7f0128f8e79ec2743e2"
-      "789290d80dc25ad7b613acd2a8e35a93e8832a69ded43484176fe95e6561431a9a54c86d48f"
-      "4cb8a7e456c1d4149002baef199b461efab8d10a0bb7503a909973ca0363a74bf7bdf88a37b"
-      "720a0e2f4c1cf7989cc3dd858e9aa9c961b0de8410732deef6c67bfc3100110643ca5aeab05"
-      "5ca3b10f1e43b11adb12bdb73216f92199826da6b3a22777b0da873b537f3a";
+      "rdqX9koea76o20SOsMdMhPpaLktftYANBgazIFQ4amvy"
+      "zTcQMOhciVuoEjhGEhqAfmi2QvTA66uGjEMAjXpMAw==";
 
     REQUIRE( outSig == EXPECTED );
 }

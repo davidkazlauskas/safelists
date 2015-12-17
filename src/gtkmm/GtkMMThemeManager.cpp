55c4cfd82589915f2d32295e477a38489eb552f3
David <david@templatious.org>
2015-12-17 20:15:44 +0200

    load stuff

--- src/gtkmm/GtkMMThemeManager.cpp
+++ src/gtkmm/GtkMMThemeManager.cpp
@@ -1,7 +1,27 @@
 
+#include <util/GenericStMessageable.hpp>
 #include "GtkMMThemeManager.hpp"
 
+TEMPLATIOUS_TRIPLET_STD;
+
 namespace SafeLists {
+
+    struct GtkMMThemeManagerImpl : public GenericStMessageable {
+        GtkMMThemeManagerImpl() {
+            regHandler(genHandler());
+        }
+
+        VmfPtr genHandler() {
+            typedef GtkMMThemeManager MNG;
+            return SF::virtualMatchFunctorPtr(
+                SF::virtualMatch< MNG::LoadTheme, const std::string >(
+                    [](ANY_CONV,const std::string& path) {
+
+                    }
+                )
+            );
+        }
+    };
 
     StrongMsgPtr GtkMMThemeManager::makeNew() {
         return nullptr;

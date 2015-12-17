4220fb0e2721dcf9944ed74f427a1e9180b1c1b3
David <david@templatious.org>
2015-12-17 19:56:08 +0200

    new sources

---
+++ src/gtkmm/GtkMMThemeManager.cpp
@@ -0,0 +1,4 @@
+
+#include "GtkMMThemeManager.hpp"
+
+
---
+++ src/gtkmm/GtkMMThemeManager.hpp
@@ -0,0 +1,10 @@
+
+#ifndef GTKMMTHEMEMANAGER_S0SW9GS4
+#define GTKMMTHEMEMANAGER_S0SW9GS4
+
+namespace SafeLists {
+
+}
+
+#endif /* end of include guard: GTKMMTHEMEMANAGER_S0SW9GS4 */
+

57536da2d23e286e63c7d88c02ef41341ff1606a
David <david@templatious.org>
2015-12-17 20:48:14 +0200

    make wnd

--- src/gtkmm/GtkMMThemeManager.cpp
+++ src/gtkmm/GtkMMThemeManager.cpp
@@ -1,4 +1,5 @@
 
+#include <gtkmm.h>
 #include <util/GenericStMessageable.hpp>
 #include "GtkMMThemeManager.hpp"
 
@@ -7,7 +8,9 @@ TEMPLATIOUS_TRIPLET_STD;
 namespace SafeLists {
 
     struct GtkMMThemeManagerImpl : public GenericStMessageable {
-        GtkMMThemeManagerImpl() {
+        GtkMMThemeManagerImpl(Gtk::Window* wnd) :
+            _wnd(wnd)
+        {
             regHandler(genHandler());
         }
 
@@ -15,12 +18,21 @@ namespace SafeLists {
             typedef GtkMMThemeManager MNG;
             return SF::virtualMatchFunctorPtr(
                 SF::virtualMatch< MNG::LoadTheme, const std::string >(
-                    [](ANY_CONV,const std::string& path) {
-
+                    [=](ANY_CONV,const std::string& path) {
+                        auto css = Gtk::CssProvider::create();
+                        css->load_from_path(path);
+                        auto screen = Gdk::Screen::get_default();
+                        auto ctx = _wnd->get_style_context();
+                        ctx->add_provider_for_screen(
+                            screen,
+                            css,
+                            GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
                     }
                 )
             );
         }
+
+        Gtk::Window* _wnd;
     };
 
     StrongMsgPtr GtkMMThemeManager::makeNew() {

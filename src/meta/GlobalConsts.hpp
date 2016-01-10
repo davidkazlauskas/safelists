1066e9c47c0e68607122ba9f7bd95ffdd38684fd
David <david@templatious.org>
2016-01-10 11:01:10 +0200

    new obj

---
+++ src/meta/GlobalConsts.hpp
@@ -0,0 +1,13 @@
+#ifndef GLOBALCONSTS_1I5CHXX9
+#define GLOBALCONSTS_1I5CHXX9
+
+#include <LuaPlumbing/messageable.hpp>
+#include <util/AutoReg.hpp>
+
+struct GlobalConsts {
+
+    // singleton
+    static StrongMsgPtr getConsts();
+};
+
+#endif /* end of include guard: GLOBALCONSTS_1I5CHXX9 */

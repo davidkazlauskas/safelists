9eb0d419e4081e8842a1a46b40f77dab989f6c7e
David <david@templatious.org>
2016-01-10 12:27:09 +0200

    meren

--- src/lua/settings.lua
+++ src/lua/settings.lua
@@ -36,32 +36,34 @@ PersistentSettings = {
 }
 
 PersistentSettings.mt = {
-    setValue = function(self,key,val)
-        if (self.settings[key] ~= val) then
-            self.revision = self.revision + 1
-            self.settings[key] = val
-        end
-    end,
-    getValue = function(self,key)
-        return self.settings[key]
-    end,
-    -- persist settings,
-    -- called on gui loop.
-    -- task of this function is to defer
-    -- save until settings stayed unmodified
-    -- for a while and then save.
-    -- if settings don't change then don't save.
-    persist = function(self)
-        if (self.revision == self.saveRevision) then
-            -- nothing to do
-            return
-        end
+    __index = {
+        setValue = function(self,key,val)
+            if (self.settings[key] ~= val) then
+                self.revision = self.revision + 1
+                self.settings[key] = val
+            end
+        end,
+        getValue = function(self,key)
+            return self.settings[key]
+        end,
+        -- persist settings,
+        -- called on gui loop.
+        -- task of this function is to defer
+        -- save until settings stayed unmodified
+        -- for a while and then save.
+        -- if settings don't change then don't save.
+        persist = function(self)
+            if (self.revision == self.saveRevision) then
+                -- nothing to do
+                return
+            end
 
-        local currTime = os.time()
-        if (currTime - self.lastSave > self.updateinterval) then
-            local toSave = JSON:encode_pretty(self.settings)
-            self.lastSave = currTime
-            self.saveFunction(toSave)
+            local currTime = os.time()
+            if (currTime - self.lastSave > self.updateinterval) then
+                local toSave = JSON:encode_pretty(self.settings)
+                self.lastSave = currTime
+                self.saveFunction(toSave)
+            end
         end
-    end
+    }
 }

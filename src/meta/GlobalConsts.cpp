120edf0984f57a40ed6bbd42e52038b251abea0c
David <david@templatious.org>
2016-01-10 11:13:34 +0200

    almost there

--- src/meta/GlobalConsts.cpp
+++ src/meta/GlobalConsts.cpp
@@ -1,7 +1,39 @@
 
+#include <util/GenericStMessageable.hpp>
 #include "GlobalConsts.hpp"
 
+TEMPLATIOUS_TRIPLET_STD;
+
+namespace {
+
+    int lookupGlobal(const std::string& path,std::string& out) {
+        return 0;
+    }
+
+}
+
 namespace SafeLists {
+
+    struct GlobalConstsImpl : public GenericStMessageable {
+        GlobalConstsImpl() {
+            regHandler(genHandler());
+        }
+
+        VmfPtr genHandler() {
+            typedef GlobalConsts GLC;
+
+            return SF::virtualMatchFunctorPtr(
+                SF::virtualMatch<
+                    GLC::LookupString, const std::string,
+                    std::string, int
+                >(
+                    [](ANY_CONV,const std::string& path,std::string& out,int& err) {
+                        err = lookupGlobal(path,out);
+                    }
+                )
+            );
+        }
+    };
 
     StrongMsgPtr GlobalConsts::getConsts() {
         return nullptr;

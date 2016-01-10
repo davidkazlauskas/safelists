c95e4ed49daaa97f78c96d6d15ae648d963a08c0
David <david@templatious.org>
2016-01-10 10:37:11 +0200

    moar stuff

--- src/lua/settings.lua
+++ src/lua/settings.lua
@@ -2,7 +2,17 @@
 JSON = require('lua/JSON')
 
 PersistentSettings = {
-    new = function()
+    new = function(reader,filename)
+        local output = {
+            revision = 0,
+            saveRevision = 0,
+            settings = {}
+        }
+    end
+}
+
+PersistentSettings.mt = {
+    setValue = function(self,key,val)
 
     end
 }

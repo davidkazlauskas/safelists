2ab06beccde8752372738035b90cf36604d0bb15
David <david@templatious.org>
2016-10-15 11:13:16 +0300

    moves moves moves

--- src/lua/main.lua
+++ src/lua/main.lua
@@ -9,29 +9,6 @@ require('genericwidget')
 require('settings')
 require('messages')
 
--- LUA HAS NO SPLIT STRING FUNCTION? ARE YOU SERIOUS?
-function string:split(delimiter)
-  local result = { }
-  local from = 1
-  local delim_from, delim_to = string.find( self, delimiter, from )
-  while delim_from do
-    table.insert( result, string.sub( self, from , delim_from-1 ) )
-    from = delim_to + 1
-    delim_from, delim_to = string.find( self, delimiter, from )
-  end
-  table.insert( result, string.sub( self, from ) )
-  return result
-end
-
-function trimString(arg)
-  return string.match(arg,"^%s*(.-)%s*$")
-end
-
-function string:ends(tail)
-    return tail == '' or
-        string.sub(self,-string.len(tail)) == tail
-end
-
 -- on select function receives integer option selected,
 -- -1 if none
 function makePopupMenuModel(context,table,onSelectFunction)
--- src/lua/util.lua
+++ src/lua/util.lua
@@ -83,3 +83,26 @@ function isValidDumbHash256(str)
 
     return true
 end
+
+-- LUA HAS NO SPLIT STRING FUNCTION? ARE YOU SERIOUS?
+function string:split(delimiter)
+  local result = { }
+  local from = 1
+  local delim_from, delim_to = string.find( self, delimiter, from )
+  while delim_from do
+    table.insert( result, string.sub( self, from , delim_from-1 ) )
+    from = delim_to + 1
+    delim_from, delim_to = string.find( self, delimiter, from )
+  end
+  table.insert( result, string.sub( self, from ) )
+  return result
+end
+
+function trimString(arg)
+  return string.match(arg,"^%s*(.-)%s*$")
+end
+
+function string:ends(tail)
+    return tail == '' or
+        string.sub(self,-string.len(tail)) == tail
+end

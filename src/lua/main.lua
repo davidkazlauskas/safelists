3b9516d05cc528ae5451b25455b8f42067eb3f0b
David <david@templatious.org>
2016-10-16 19:37:34 +0300

    factor out menu

--- src/lua/main.lua
+++ src/lua/main.lua
@@ -9,28 +9,7 @@ require('safelist-constants')
 require('genericwidget')
 require('settings')
 require('messages')
-
--- on select function receives integer option selected,
--- -1 if none
-function makePopupMenuModel(context,table,onSelectFunction)
-    local theModel = table
-
-    local menuModelHandler = context:makeLuaMatchHandler(
-        VMatch(function(natpack,val)
-            local num = val:values()._2 + 1
-            natpack:setSlot(3,VString(theModel[num]))
-        end,"MWI_PMM_QueryItem"),
-        VMatch(function(natpack,val)
-            natpack:setSlot(2,VInt(#theModel))
-        end,"MWI_PMM_QueryCount","int"),
-        VMatch(function(natPack,val)
-            local res = val:values()._2
-            onSelectFunction(res)
-        end,"MWI_PMM_OutSelected","int")
-    )
-
-    return menuModelHandler
-end
+require('guiutil')
 
 ObjectRetainer = {
     __index = {

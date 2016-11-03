067b02c0a2c3cb91445d9af6443536d1083ac80b
David <david@templatious.org>
2016-11-03 19:22:58 +0200

    case sensitive name check

--- src/lua/sqlite.lua
+++ src/lua/sqlite.lua
@@ -17,11 +17,18 @@ function sqlCheckForForbiddenFileNames(dirId,fileName)
     return    " SELECT CASE"
            .. " WHEN (EXISTS (SELECT file_name FROM files"
            .. "     WHERE dir_id=" .. dirId
-           .. "     AND file_name='" .. fileName .. "')) THEN 1"
+           .. "     AND lower(file_name)=lower('" .. fileName .. "'))) THEN 1"
            .. " WHEN (EXISTS (SELECT file_name FROM files"
            .. "     WHERE dir_id=" .. dirId
-           .. "     AND (file_name || '.ilist' ='" .. fileName .. "'"
-           .. "     OR file_name || '.ilist.tmp' ='" .. fileName .. "'))) THEN 2"
+           .. "     AND (lower(file_name || '.ilist') = lower('" .. fileName .. "')"
+           .. "     OR lower(file_name || '.ilist.tmp') = lower('" .. fileName .. "')))) THEN 2"
+           .. " WHEN (EXISTS (SELECT dir_name FROM directories"
+           .. "     WHERE dir_parent=" .. dirId
+           .. "     AND lower(dir_name)=lower('" .. fileName .. "'))) THEN 3"
+           .. " WHEN (EXISTS (SELECT dir_name FROM directories"
+           .. "     WHERE dir_parent=" .. dirId
+           .. "     AND (lower(dir_name || '.ilist') = lower('" .. fileName .. "')"
+           .. "     OR lower(dir_name || '.ilist.tmp') = lower('" .. fileName .. "')))) THEN 4"
            .. " ELSE 0"
            .. " END;"
 end
